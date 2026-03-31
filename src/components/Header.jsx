61975b75fd312408a85fec18f86e5b110b5316fb
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:31:03 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <>
+      <div id="container">
+        <button id="para1">Previous</button>
+        <button id="para2">1</button>
+        <button id="para4">2</button>
+        <button id="para4">3</button>
+        <button id="para4">4</button>
+        <button id="para4">5</button>
+        <button id="para4">6</button>
+        <button id="para4">7</button>
+        <button id="para4">8</button>
+        <button id="para5">Next</button>
+      </div>
+    </>
+  );
+}
